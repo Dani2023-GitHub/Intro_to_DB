a9885b93f60f40df906ea692d467d78aa2ae21e8
Daniel Getachew <dani.gechoo@gmail.com>
2024-10-21 22:09:53 +0300

    task_5 added

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 USE alx_book_store;
-INSERT INTO customer(customerid, customer_name, email, address)
+INSERT INTO customer(customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baido', 'cbaidoo@sanstech.com','123 Happiness Ave.');`
